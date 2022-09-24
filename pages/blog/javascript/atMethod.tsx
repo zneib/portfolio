8809fc8091d391971ebe80e40dd69beaa681da71
Zach Neibaur <zneibaur@protonmail.com>
2022-09-24 08:39:18 -0600

    Another blog post test

--- pages/blog/javascript/atMethod.tsx
+++ pages/blog/javascript/atMethod.tsx
@@ -7,25 +7,28 @@ import twitter from '../../../public/twitter.svg';
 import github from '../../../public/github.svg';
 
 const Wrapper = styled.div`
-  display: flex;
   padding: 50px 100px;
   height: 100vh;
+  @media (max-width: 768px) {
+    padding: 50px 25px;
+  }
 `
 const Content = styled.section`
-  flex: 1;
-  width: 100%;
+  width: 60%;
   padding-top: 50px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `
 const ImageContainer = styled.div`
   width: 500px;
-`
-const TopSection = styled.section`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  color: #000;
-  @media (prefers-color-scheme: dark) {
-    color: var(--gray-ten);
+  max-width: 500px;
+  margin: 25px auto;
+  @media (max-width: 768px) {
+    width: 100%;
   }
 `
 const SocialLinks = styled.div`
@@ -34,34 +37,27 @@ const SocialLinks = styled.div`
   }
 `
 
-const LeftSection = styled.div`
-  flex: 1;
-`
-const RightSection = styled.div`
-  flex: 1;
-  text-align: right;
+const TopSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media (prefers-color-scheme: dark) {
+    color: var(--gray-ten);
+  }
 `
 
 const AtMethod: NextPage = () => {
   return (
     <Wrapper>
-      <LeftSection>
-      <Link href="/blog">
-        <a style={{ display: 'flex', alignItems: 'center' }}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-          </svg>
-          <h2 style={{ display: 'inline-block', margin: '0 0 0 5px' }}>Blog</h2>
-        </a>
-      </Link>
-      </LeftSection>
-      <Content>
-        <p>Using the <code>at()</code> method to find the last item in an array:</p>
-        <ImageContainer>
-          <Image src={imageOne} alt="Code Example" />
-        </ImageContainer>
-      </Content>
-      <RightSection>
+      <TopSection>
+        <Link href="/blog">
+          <a style={{ display: 'flex', alignItems: 'center' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+            </svg>
+            <h2 style={{ display: 'inline-block', margin: '0 0 0 5px' }}>Blog</h2>
+          </a>
+        </Link>
         <SocialLinks>
           <a href="https://github.com/zneib">
             <Image src={github} alt="source control" />
@@ -70,7 +66,13 @@ const AtMethod: NextPage = () => {
             <Image src={twitter} alt="social" />
           </a>
         </SocialLinks>
-      </RightSection>
+      </TopSection>
+      <Content>
+        <p>Using the <code>at()</code> method to find the last item in an array:</p>
+        <ImageContainer>
+          <Image src={imageOne} alt="Code Example" />
+        </ImageContainer>
+      </Content>
     </Wrapper>
   )
 }
