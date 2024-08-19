507c8cb1595d71f6d178a92c03381aeb7e88f321
Zach Neibaur <zneibaur@protonmail.com>
2024-08-19 09:35:27 -0600

    Trying out some new styles

--- pages/popoverPost.tsx
+++ pages/popoverPost.tsx
@@ -8,10 +8,19 @@ const Page = styled.section`
   height: 100vh;
   padding-top: 25px;
 `
+const ContentWrapper = styled.div`
+  width: 50%;
+  margin: 20px auto;
+  background-color: #fafafa;
+  padding: 5px 15px;
+  border-radius: 5px;
+  box-shadow: 0px 4px 8px rgba(0,0,0,0.2);
+`
 const Text = styled.article`
   margin: 20px auto;
-  width: 50%;
-  line-height: 20px;
+  width: 100%;
+  letter-spacing: .2px;
+  line-height: 23px;
   & > a {
     text-decoration: underline;
   }
@@ -20,10 +29,13 @@ const ImageWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 50%;
+  width: 90%;
   margin: 0 auto;
   & > img {
     width: 100%;
+    height: auto;
+    border-radius: 5px;
+    margin-top: 15px;
   }
   @media screen and (max-width: 375px) {
     width: 80%;
@@ -31,7 +43,7 @@ const ImageWrapper = styled.div`
 `
 const TextCentered = styled.article`
   margin: 20px auto;
-  width: 50%;
+  width: 100%;
   line-height: 20px;
   text-align: center;
   & > a {
@@ -56,17 +68,20 @@ const PopOverPost: NextPageWithLayout = () => {
       <Link href="/blog">
         <BackButton>Back</BackButton>
       </Link>
-      <Text>One of my favorite new things in HTML has landed this year. The Popover API. I thought the <CodeSnippet codeString='dialog' /> element was already super handy, but I see myself using the Popover API even more now that it's fully supported across all browsers.</Text>
-      <Text>As of April 2024 this new API is available across all major desktop and mobile browsers. So right now, we can use these new features everywhere in updated browsers.</Text>
-      <Text>What makes this new API so awesome? So many times in frontend development we need to display a little bit of information to the user.
-      It might be that file upload successfully, or something is missing on a form. These extra little information windows can really help add some clarity to what is happening on our pages.
-      Modern frameworks and libraries have helped make these kinds of things pretty easy to bake in most of the time. But sometimes, the CSS can still be a little tricky to get right each time.</Text>
-
-      <Text>Here's a super basic example -</Text>
-      <ImageWrapper>
-        <Image src={basic} alt="Basic Popover" style={{ objectFit: 'contain' }} />
-      </ImageWrapper>
-      <TextCentered>Try it out on <Link href="https://codepen.io/zneib/pen/MWMrLKz" target="_blank" >CodePen</Link></TextCentered>
+      <ContentWrapper>
+        <Text>One of my favorite new things in HTML has landed this year. The Popover API. I thought the <CodeSnippet codeString='dialog' /> element was already super handy, but I see myself using the Popover API even more now that it's fully supported across all browsers.</Text>
+        <Text>As of April 2024 this new API is available across all major desktop and mobile browsers. So right now, we can use these new features everywhere in updated browsers.</Text>
+        <Text>What makes this new API so awesome? So many times in frontend development we need to display a little bit of information to the user.
+        It might be that file upload successfully, or something is missing on a form. These extra little information windows can really help add some clarity to what is happening on our pages.
+        Modern frameworks and libraries have helped make these kinds of things pretty easy to bake in most of the time. But sometimes, the CSS can still be a little tricky to get right each time.</Text>
+        <Text>Here's a super basic example -</Text>
+      </ContentWrapper>
+      <ContentWrapper>
+        <ImageWrapper>
+          <Image src={basic} alt="Basic Popover" style={{ objectFit: 'contain' }} />
+        </ImageWrapper>
+        <TextCentered>Try it out on <Link href="https://codepen.io/zneib/pen/MWMrLKz" target="_blank" >CodePen</Link></TextCentered>
+      </ContentWrapper>
     </Page>
   )
 }
